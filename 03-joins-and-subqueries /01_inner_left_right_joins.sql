2034d531e2def35110c620578d72270a37a8e6a8
Suryaa V S <83962899+stylishsuryaa@users.noreply.github.com>
2026-09-01 16:21:17 +0530

    Update 01_inner_left_right_joins.sql

--- 03-joins-and-subqueries /01_inner_left_right_joins.sql
+++ 03-joins-and-subqueries /01_inner_left_right_joins.sql
@@ -1,1 +1,16 @@
+USE company_db;
 
+-- 1. INNER JOIN (Matching records only)
+SELECT employees.emp_name, departments.dept_name, employees.salary
+FROM employees
+INNER JOIN departments ON employees.dept_id = departments.dept_id;
+
+-- 2. LEFT JOIN (All employees, even without a department)
+SELECT employees.emp_name, departments.dept_name
+FROM employees
+LEFT JOIN departments ON employees.dept_id = departments.dept_id;
+
+-- 3. RIGHT JOIN (All departments, even with no employees)
+SELECT departments.dept_name, employees.emp_name
+FROM employees
+RIGHT JOIN departments ON employees.dept_id = departments.dept_id;
