3ea18bc0400fac465c52444b63fef2dcb2dba1b6
Suryaa V S <83962899+stylishsuryaa@users.noreply.github.com>
2026-09-01 16:22:00 +0530

    Update 02_aggregate_functions_metrics.sql

--- 04-aggregations-and-grouping/02_aggregate_functions_metrics.sql
+++ 04-aggregations-and-grouping/02_aggregate_functions_metrics.sql
@@ -1,1 +1,10 @@
+USE company_db;
 
+-- Global Aggregation Metrics
+SELECT 
+    COUNT(*) AS total_employees,
+    MIN(salary) AS minimum_salary,
+    MAX(salary) AS maximum_salary,
+    ROUND(AVG(salary), 2) AS average_salary,
+    SUM(salary) AS total_company_payroll
+FROM employees;
