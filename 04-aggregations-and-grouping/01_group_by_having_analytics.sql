05910b82a2dabf4c95bc5a2838a099ccae01bc9c
Suryaa V S <83962899+stylishsuryaa@users.noreply.github.com>
2026-09-01 16:21:50 +0530

    Update 01_group_by_having_analytics.sql

--- 04-aggregations-and-grouping/01_group_by_having_analytics.sql
+++ 04-aggregations-and-grouping/01_group_by_having_analytics.sql
@@ -1,1 +1,11 @@
+USE company_db;
 
+-- Department summary with minimum employee threshold
+SELECT 
+    dept_id,
+    COUNT(emp_id) AS total_employees,
+    AVG(salary) AS average_salary,
+    SUM(salary) AS total_payroll
+FROM employees
+GROUP BY dept_id
+HAVING COUNT(emp_id) >= 1;
