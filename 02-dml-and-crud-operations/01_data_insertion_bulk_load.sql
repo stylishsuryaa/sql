9788542dcc0b8079b6ad8561ef6b7e3444eae8b7
Suryaa V S <83962899+stylishsuryaa@users.noreply.github.com>
2026-09-01 16:20:47 +0530

    Update 01_data_insertion_bulk_load.sql

--- 02-dml-and-crud-operations/01_data_insertion_bulk_load.sql
+++ 02-dml-and-crud-operations/01_data_insertion_bulk_load.sql
@@ -1,1 +1,17 @@
+USE company_db;
 
+-- 1. Single Insert
+INSERT INTO departments (dept_name) VALUES ('IT');
+
+-- 2. Bulk Insert (Multiple rows)
+INSERT INTO departments (dept_name) VALUES 
+('HR'),
+('Finance'),
+('Marketing');
+
+-- 3. Insert Employees Data
+INSERT INTO employees (emp_name, email, salary, dept_id) VALUES 
+('Suryaa', 'suryaa@example.com', 75000.00, 1),
+('Anitha', 'anitha@example.com', 70000.00, 1),
+('Karthik', 'karthik@example.com', 50000.00, 2),
+('Priya', 'priya@example.com', 60000.00, 3);
