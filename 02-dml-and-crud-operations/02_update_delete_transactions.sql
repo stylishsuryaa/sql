186d67b7ca6b5d9c76d8a300ed594effd9f72d75
Suryaa V S <83962899+stylishsuryaa@users.noreply.github.com>
2026-09-01 16:21:04 +0530

    Update 02_update_delete_transactions.sql

--- 02-dml-and-crud-operations/02_update_delete_transactions.sql
+++ 02-dml-and-crud-operations/02_update_delete_transactions.sql
@@ -1,1 +1,21 @@
+USE company_db;
 
+-- 1. Update Query
+UPDATE employees 
+SET salary = 80000.00 
+WHERE emp_name = 'Suryaa';
+
+-- 2. Delete Query
+DELETE FROM employees 
+WHERE emp_name = 'Priya';
+
+-- 3. Safe Transaction Block
+START TRANSACTION;
+
+UPDATE employees SET salary = salary + 3000 WHERE dept_id = 1;
+
+-- Save permanently
+COMMIT;
+
+-- Undo changes if needed:
+-- ROLLBACK;
