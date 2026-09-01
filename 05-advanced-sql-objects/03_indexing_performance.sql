e56317b4d52a77901e21ac6f374fe06697c5874b
Suryaa V S <83962899+stylishsuryaa@users.noreply.github.com>
2026-09-01 16:22:56 +0530

    Update 03_indexing_performance.sql

--- 05-advanced-sql-objects/03_indexing_performance.sql
+++ 05-advanced-sql-objects/03_indexing_performance.sql
@@ -1,1 +1,13 @@
+USE company_db;
 
+-- 1. Create single index on email
+CREATE INDEX idx_emp_email ON employees(email);
+
+-- 2. Create composite index on department & salary
+CREATE INDEX idx_dept_salary ON employees(dept_id, salary);
+
+-- 3. Check query execution plan
+EXPLAIN SELECT * FROM employees WHERE email = 'suryaa@example.com';
+
+-- 4. List all active indexes
+SHOW INDEX FROM employees;
