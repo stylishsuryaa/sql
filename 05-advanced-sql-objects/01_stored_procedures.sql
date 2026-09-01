ccdfdf17c0fc9e9236ffbd3df16bcc2f17fd97d5
Suryaa V S <83962899+stylishsuryaa@users.noreply.github.com>
2026-09-01 16:22:27 +0530

    Update 01_stored_procedures.sql

--- 05-advanced-sql-objects/01_stored_procedures.sql
+++ 05-advanced-sql-objects/01_stored_procedures.sql
@@ -1,1 +1,16 @@
+USE company_db;
 
+DELIMITER //
+
+-- Procedure to fetch employees by department ID
+CREATE PROCEDURE GetEmployeesByDept(IN p_dept_id INT)
+BEGIN
+    SELECT emp_id, emp_name, salary 
+    FROM employees 
+    WHERE dept_id = p_dept_id;
+END //
+
+DELIMITER ;
+
+-- Execute the procedure:
+CALL GetEmployeesByDept(1);
